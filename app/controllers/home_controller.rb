5f0041285aaa847dc5be3e696809260e1266e560
Keith Turner <turner@keithturner.com>
2018-05-18 10:38:50 -0600

    Added Faker Gem to Gemfile, Created instance variables in controller, called in the view.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,18 @@
 class HomeController < ApplicationController
+
+  before_action :common, :only => [:index, :team]
+
+  def common
+    @hero_name = Faker::Superhero.name # Random Superhero name
+    @hero_power = Faker::Superhero.power # Random Superhero power
+    @hero_prefix = Faker::Superhero.prefix # Random Superhero prefix
+  end
+
   def index
+    @name = "Xander Alexander"
   end
 
   def team
-    
+
   end
 end
